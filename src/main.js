f87e40996f6ce71c4da3a18ef47ffb8aa06bb9ce
cheekiam <czkiam@gmail.com>
2018-09-28 18:43:52 +0800

    pump f7 version to 3.3.2 and vue-template-comipiler to align with vue core. fix showstopper

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 // Import Vue
-import Vue from 'vue';
+import Vue from 'vue'
 
 // Import F7
-import Framework7 from 'framework7/framework7.esm.bundle.js';
+import Framework7 from 'framework7/framework7.esm.bundle.js'
 
 // Import F7 Vue Plugin
-import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js';
+import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js'
 
 // Import F7 Styles
-import Framework7Styles from 'framework7/css/framework7.css';
+import Framework7Styles from 'framework7/css/framework7.css'
+
 
 // Import Icons and App Custom Styles
 import IconsStyles from './css/icons.css';
@@ -17,8 +18,11 @@ import AppStyles from './css/app.css';
 // Import App Component
 import App from './app';
 
-//Init F7 Vue Plugin
+// Init F7 Vue Plugin
 Framework7.use(Framework7Vue)
+Framework7.use(Framework7Styles)
+Framework7.use(IconsStyles)
+Framework7.use(AppStyles)
 
 // Init App
 new Vue({
